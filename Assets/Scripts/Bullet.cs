28c502f9e0ea298cacc4f3ba4761af0f7601e663
Tamir Yamaleyev <tamirka8@gmail.com>
2025-11-24 16:37:56 +0200

    Small balance changes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [Header("Projectile Settings")]
-    public float speed = 20f;
+    public float speed = 150f;
     public float lifetime = 5f;  // Destroy after this time
 
     private float damage;
